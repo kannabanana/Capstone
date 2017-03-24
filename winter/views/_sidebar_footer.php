b678ed1b832d863324cee25f3fa0dc84280cb815
Sr Kanna <kannas@flip3.engr.oregonstate.edu>
2017-03-23 20:43:34 -0700

    security check

--- winter/views/_sidebar_footer.php
+++ winter/views/_sidebar_footer.php
@@ -1,9 +1,10 @@
  <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
-                    </div>
+                   </div>
                 </div>
             </div>
         </div>
         <!-- /#page-content-wrapper -->
+<a href="http://www.beyondsecurity.com/vulnerability-scanner-verification/web.engr.oregonstate.edu"><img src="https://seal.beyondsecurity.com/verification-images/web.engr.oregonstate.edu/vulnerability-scanner-2.gif" alt="Website Security Test" border="0" /></a>
 
     </div>
     <!-- /#wrapper -->
