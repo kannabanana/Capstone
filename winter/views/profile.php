17d672c166a709ba1281665af20a9808365dab68
Sr Kanna <kannas@flip3.engr.oregonstate.edu>
2017-04-29 20:56:22 -0700

    comments

--- winter/views/profile.php
+++ winter/views/profile.php
@@ -1,4 +1,6 @@
 <?php 
+
+	//include header or sidebar for navigation and database access
           include("_header.php"); 
 	  include("_sidebar_header.php");
 
@@ -7,6 +9,8 @@
 
 <html>
 <head>
+
+<!--this is for css and javascript-->
 	<meta name="viewport" content="width=device-width, initial-scale=1" content="height=device-height, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
@@ -19,7 +23,7 @@
 <body>
 <?php
 if(isset($_SESSION['uid']) && !empty($_SESSION['uid'])){
-$id = $_SESSION['uid'];
+$id = $_SESSION['uid'];		//check if session id exists, redirect otherwise
 
 }
 else{
@@ -29,7 +33,7 @@ header("Location: landing.php");
 exit();
 }
 if($result = $db->query("select * from employee_information where user_id = '$id'")){
-		while($obj = $result->fetch_object()){
+		while($obj = $result->fetch_object()){					//query for employee information based on a given id and display first name, last name, user name, phone number etc.
 			$first = htmlspecialchars($obj->first_name);
 			$last = htmlspecialchars($obj->last_name);
 			$user = htmlspecialchars($obj->user_name);
@@ -49,7 +53,7 @@ if($result = $db->query("select * from employee_information where user_id = '$id
       <div class="col-md-5  toppad  pull-right col-md-offset-3 ">
            <!--<A href="edit.html" >Edit Profile</A> -->    
 
-        <A href="submit_logout.php" >Logout</A>
+        <A href="submit_logout.php" >Logout</A>		<!--give them the option to logout-->
        <br>
       </div>
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xs-offset-0 col-sm-offset-0 col-md-offset-3 col-lg-offset-3 toppad" >
@@ -61,6 +65,7 @@ if($result = $db->query("select * from employee_information where user_id = '$id
             </div>
             <div class="panel-body">
               <div class="row">     
+				<!--displaying the information gathered above-->
 				<?php if(isset($image_data) && !empty($image_data)){?>
 				<div class="col-md-4 col-lg-4 " align="center"> <img alt="User Pic" src="data:image/jpeg;base64,<?php echo base64_encode( $image_data ); ?>" class="img-circle img-responsive" /> </div>
 			  <?php }?>                <div class=" col-md-8 col-lg-8 " align="right"> 
@@ -68,7 +73,7 @@ if($result = $db->query("select * from employee_information where user_id = '$id
                     <tbody>
                       <tr>
                         <td>Email:</td>
-                        <td><a href="mailto:<?php echo $email; ?> "><?php echo $email;  ?></a> </td>
+                        <td><a href="mailto:<?php echo $email; ?> "><?php echo $email;  ?></a> </td>			<!--displaying email, phone, major, hiredat, end date etc-->
                       </tr>
                       <tr>
                         <td>Phone:</td>
@@ -96,13 +101,6 @@ if($result = $db->query("select * from employee_information where user_id = '$id
                 </div>
               </div>
             </div>
-                 <!--<div class="panel-footer">
-                        <a data-original-title="Broadcast Message" data-toggle="tooltip" type="button" class="btn btn-sm btn-primary"><i class="glyphicon glyphicon-envelope"></i></a>
-                        <span class="pull-right">
-                            <a href="edit.html" data-original-title="Edit this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-warning"><i class="glyphicon glyphicon-edit"></i></a>
-                            <a data-original-title="Remove this user" data-toggle="tooltip" type="button" class="btn btn-sm btn-danger"><i class="glyphicon glyphicon-remove"></i></a>
-                        </span>
-                    </div>-->
             
           </div>
         </div>
